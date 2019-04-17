bbd39a5a716c8663345e8bbb7b932f2b2bef1cfa
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-04-17 13:58:50 +0100

    Add files via upload

---
+++ src/parser.py
@@ -0,0 +1,120 @@
+import argparse
+
+def parameter_parser():
+
+    """
+    A method to parse up command line parameters. By default it gives an embedding of the Wikipedia Chameleons dataset.
+    The default hyperparameters give a good quality representation without grid search.
+    Representations are sorted by ID.
+    """
+
+    parser = argparse.ArgumentParser(description = "Run MUSAE/AE.")
+
+
+    parser.add_argument('--graph-input',
+                        nargs = '?',
+                        default = "./input/edges/chameleon_edges.csv",
+	                help = 'Input folder with jsons.')
+
+    parser.add_argument('--features-input',
+                        nargs = '?',
+                        default = "./input/features/chameleon_features.json",
+	                help = 'Input folder with jsons.')
+
+    parser.add_argument('--output',
+                        nargs = '?',
+                        default = './output/chameleon_embedding.csv',
+	                help = 'Embeddings path.')
+
+    parser.add_argument('--log',
+                        nargs = '?',
+                        default = './logs/chameleon.json',
+	                help = 'Embeddings path.')
+
+    parser.add_argument('--dimensions',
+                        type = int,
+                        default = 24,
+	                help = 'Number of dimensions. Default is 16.')
+
+    parser.add_argument('--walk-number',
+                        type = int,
+                        default = 5,
+	                help = 'Number of walks. Default is 5.')
+
+    parser.add_argument('--walk-length',
+                        type = int,
+                        default = 80,
+	                help = 'Walk length. Default is 80.')
+
+    parser.add_argument('--base-model',
+                        nargs = '?',
+                        default = 'null',
+	                help = 'Random walk order.')
+
+    parser.add_argument('--model',
+                        nargs = '?',
+                        default = 'musae',
+	                help = 'Random walk order.')
+
+    parser.add_argument('--sampling',
+                        nargs = '?',
+                        default = 'first',
+	                help = 'Random walk order.')
+
+    parser.add_argument('--P',
+                        type = float,
+                        default = 1.0,
+	                help = 'Number of walks. Default is 1.0.')
+
+    parser.add_argument('--Q',
+                        type = float,
+                        default = 1.0,
+	                help = 'Number of walks. Default is 1.0.')
+
+    parser.add_argument('--down-sampling',
+                        type = float,
+                        default = 0.001,
+	                help = 'Number of walks. Default is 0.001.')
+
+    parser.add_argument('--exponent',
+                        type = float,
+                        default = 0.75,
+	                help = 'Number of walks. Default is 0.001.')
+
+    parser.add_argument('--alpha',
+                        type = float,
+                        default = 0.05,
+	                help = 'Number of walks. Default is 0.05.')
+
+    parser.add_argument('--min_alpha',
+                        type = float,
+                        default = 0.025,
+	                help = 'Number of walks. Default is 0.025.')
+
+    parser.add_argument('--approximation-order',
+                        type = int,
+                        default = 3,
+	                help = 'Number of neighbor embeddings. Default is 3.')
+
+    parser.add_argument('--min-count',
+                        type = int,
+                        default = 1,
+	                help = 'Number of neighbor embeddings. Default is 3.')
+
+    parser.add_argument('--negative-samples',
+                        type = int,
+                        default = 5,
+	                help = 'Number of neighbor embeddings. Default is 5.')
+
+    parser.add_argument('--workers',
+                        type = int,
+                        default = 4,
+	                help = 'Number of cores. Default is 4.')
+
+    parser.add_argument('--epochs',
+                        type = int,
+                        default = 5,
+	                help = 'Number of epochs. Default is 5.')
+
+    return parser.parse_args()
+
---
+++ src/utils.py
@@ -0,0 +1,42 @@
+import networkx as nx
+import pandas as pd
+import json
+import random
+from texttable import Texttable
+from gensim.models.doc2vec import TaggedDocument
+
+def tab_printer(args):
+    """
+    Function to print the logs in a nice tabular format.
+    :param args: Parameters used for the model.
+    """
+    args = vars(args)
+    keys = sorted(args.keys())
+    t = Texttable() 
+    t.add_rows([["Parameter", "Value"]] + [[k.replace("_"," ").capitalize(),args[k]] for k in keys])
+    print(t.draw())
+
+def load_graph(graph_path):
+    """
+    """
+    data = pd.read_csv(graph_path)
+    edges = data.values.tolist()
+    edges = [[int(edge[0]),int(edge[1])] for edge in edges]
+    graph = nx.from_edgelist(edges)
+    graph.remove_edges_from(graph.selfloop_edges())
+    return graph
+
+def load_features(features_path):
+    """
+    """
+    features = json.load(open(features_path))
+    features = {str(k): [str(val) for val in v] for k,v in features.items()}
+    return features
+
+def create_documents(features):
+    """
+    """
+    docs = [TaggedDocument(words = v, tags = [str(k)]) for k, v in features.items()]
+
+    return docs
+
